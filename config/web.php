b9f0da48056861d362a3e4225b09e2aaf3a0f9f0
Poremchuk Evgeniy <e.poremchuk@gmail.com>
2015-09-09 12:00:41 +0300

    Add API Controller and delete db_sample.php

--- config/web.php
+++ config/web.php
@@ -11,6 +11,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '3jWUubF1ovaTpTuGALlHgsdbw1XpW_zGEG',
+                'parsers' => [
+                    'application/json' => 'yii\web\JsonParser',
+                ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
